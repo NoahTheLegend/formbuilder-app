e4c2c363499a150b05b61f76a738170b54e17a85
NoahTheLegend <20jeka03@gmail.com>
2024-09-16 22:23:38 +0300

    researching

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Http\Request;
+ 
+Route::get('/user/{id}', function (Request $request, string $id) {
+    return 'User '.$id.' a '.$request->acceptsHtml();
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
