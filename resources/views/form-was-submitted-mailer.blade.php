00a8f76bd458c5e1f57a81d1aa0ec5ecd4bdec02
NoahTheLegend <20jeka03@gmail.com>
2024-09-18 21:25:41 +0300

    fix mailer

--- resources/views/form-was-submitted-mailer.blade.php
+++ resources/views/form-was-submitted-mailer.blade.php
@@ -5,10 +5,8 @@
 </head>
 <body>
     <h1>A form was submitted</h1>
-    <p><strong>First Name:</strong> {{ $formData['first_name'] }}</p>
-    <p><strong>Last Name:</strong> {{ $formData['last_name'] }}</p>
-    <p><strong>Phone:</strong> {{ $formData['phone'] }}</p>
-    <p><strong>Comment:</strong> {{ $formData['comment'] }}</p>
-    <p><strong>Photo:</strong> <a href="{{ asset('storage/' . $formData['photo']) }}">photo</a></p>
+    @foreach ($formData as $data)
+        <p>{{$data}}</p>
+    @endforeach
 </body>
 </html>
